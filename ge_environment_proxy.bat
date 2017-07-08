c89d8d19fe3804ca444fad251ed5c14fbad8ef20
aarontcopal2 <aarontcopal2@gmail.com>
2017-07-08 14:22:48 +0530

    added 2 batch files to setup prxy settings

---
+++ ge_environment_proxy.bat
@@ -0,0 +1,16 @@
+@REM is used to comment a line
+
+@REM These are some practise commands
+@REM echo %windir%
+@REM dir %windir%
+@REM %ProgramFiles(x86)%
+
+@REM These are the system variables to be set for ge environment
+set http_proxy=http://cis-india-pitc-bangalore.corporate.ge.com:80
+set https_proxy=http://cis-india-pitc-bangalore.corporate.ge.com:80
+set HTTPS_PROXY=http://cis-india-pitc-bangalore.corporate.ge.com:80
+set HTTP_PROXY=http://cis-india-pitc-bangalore.corporate.ge.com:80 
+set NO_PROXY=localhost, devcloud.sw.ge.com, openge.ge.com, github.sw.ge.com, github.build.ge.com,indiavoice.corporate.ge.com,*.predix.io,*.predix.com
+
+@REM set MAVEN_HOME=C:\Program Files\apache-maven-3.5.0
+@REM set M2_HOME=C:\Program Files\apache-maven-3.5.0
