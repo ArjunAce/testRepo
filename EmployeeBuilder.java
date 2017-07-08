06b7ff82a39f9a72380990ed6ec2d018d76c44d8
aarontcopal2 <aarontcopal2@gmail.com>
2017-07-08 17:28:20 +0530

    we have completed the implementation of ssoid,name and projectID in employeeBuilder

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -2,8 +2,6 @@ package p1;
 public class EmployeeBuilder {
 	private int ssoid;
 	private String name;
-	private String gender;
-	private int age;
 	private int projectid;
 	public EmployeeBuilder setSsoid(int ssoid) {
 		this.ssoid = ssoid;
@@ -11,14 +9,6 @@ public class EmployeeBuilder {
 	}
 	public EmployeeBuilder setName(String name) {
 		this.name = name;
-		return this;
-	}
-	public EmployeeBuilder setGender(String gender) {
-		this.gender = gender;
-		return this;
-	}
-	public EmployeeBuilder setAge(int age) {
-		this.age = age;
 		return this;
 	}
 	public EmployeeBuilder setProjectid(int projectid) {
