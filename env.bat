b7fd8341d267e8bb41914528dcba5aca4b330a5e
aarontcopal2 <aarontcopal2@gmail.com>
2017-07-08 13:13:23 +0530

    adding the script file as well another txt file to the repo

---
+++ env.bat
@@ -0,0 +1,14 @@
+@REM These are some practise commands
+
+@REM echo %windir%
+@REM dir %windir%
+@REM %ProgramFiles(x86)%
+
+@REM set http_proxy=http://cis-india-pitc-bangalore.corporate.ge.com:80
+@REM set https_proxy=http://cis-india-pitc-bangalore.corporate.ge.com:80
+@REM set HTTPS_PROXY=http://cis-india-pitc-bangalore.corporate.ge.com:80
+@REM set HTTP_PROXY=http://cis-india-pitc-bangalore.corporate.ge.com:80 
+@REM set NO_PROXY=localhost, devcloud.sw.ge.com, openge.ge.com, github.sw.ge.com, github.build.ge.com,indiavoice.corporate.ge.com,*.predix.io,*.predix.com
+
+set MAVEN_HOME=C:\Program Files\apache-maven-3.5.0
+set M2_HOME=C:\Program Files\apache-maven-3.5.0
